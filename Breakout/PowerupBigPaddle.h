aa38baf5516183518f0a1fea7482b28df0c64446
beaparsons <157722909+beaparsons@users.noreply.github.com>
2025-10-20 16:13:14 +0100

    Add files via upload

--- Breakout/PowerupBigPaddle.h
+++ Breakout/PowerupBigPaddle.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "PowerupManager.h"
-
+#include "PowerupBase.h"
+//class PowerupBase {};
 class PowerupBigPaddle : public PowerupBase
 {
 public:
     PowerupBigPaddle(sf::RenderWindow* window, Paddle* paddle, Ball* ball);
     ~PowerupBigPaddle();
 
-    std::pair<POWERUPS, float> applyEffect() override; // Method to apply the power-up effect
+    std::pair<POWERUPS, float> applyEffect(); //override; // Method to apply the power-up effect
 };
--- Breakout/PowerupFireBall.h
+++ Breakout/PowerupFireBall.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "PowerupBase.h"
+//class PowerupBase{};
 class PowerupFireBall : public PowerupBase
 {
 public:
     PowerupFireBall(sf::RenderWindow* window, Paddle* paddle, Ball* ball);
     ~PowerupFireBall();
 
-    std::pair<POWERUPS, float> applyEffect() override; // Method to apply the power-up effect
+    std::pair<POWERUPS, float> applyEffect(); //override; // Method to apply the power-up effect
 
 };
 
